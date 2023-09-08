e85826de3b0b0ee7870b61a8f03ccf883430b9a9
MassimoAzzini <massimo.azzini.web@gmail.com>
2023-09-08 16:21:20 +0200

    [add]: const js

---
+++ js/script.js
@@ -0,0 +1,17 @@
+console.log('ciao');
+
+const firstName = document.getElementById('first-name').innerHTML;
+const lastName = document.getElementById('last-name').innerHTML;
+const km = document.getElementById('km').innerHTML;
+const age = document.getElementById('age').innerHTML;
+
+const discountUnderage = 20;
+const discountOver65 = 40;
+
+
+
+console.log(firstName);
+console.log(lastName);
+console.log(km);
+console.log(age);
+
