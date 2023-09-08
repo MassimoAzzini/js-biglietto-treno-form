994a30a2fbcf41d65778ff7d2ac9ef3c2d42d196
MassimoAzzini <massimo.azzini.web@gmail.com>
2023-09-09 01:25:15 +0200

    [add]: form html and linked to JS

--- js/script.js
+++ js/script.js
@@ -10,39 +10,32 @@ const discountUnderage = 20;
 const discountOver65 = 40;
 const price = km * priceKm;
 
-
-// let insertOk = true
-// let messageKm;
+let finalPrice;
 let discountMessage;
-let code
+let code;
 
 console.log(firstName);
 console.log(lastName);
 console.log(km);
 console.log(age);
 
-// if(isNaN(km)){
-//   insertOk = false
-//   messageKm = 'Errore! Devi inserire un numero nel campo chilometri';
+const button1 = document.getElementById('btn-1');
+const button2 = document.getElementById('btn-2');
+const emissioneBiglietto = document.getElementById('emissione-biglietto');
 
-// }else if(km < 1){
-//   insertOk = false
-//   messageKm = 'Errore! Inserire un numero superiore a 1';
-// }
 
 if(age == 'Maggiorenne'){
-  
+      
   finalPrice = price;
   discountMessage = `Biglietto Standard`
-
-
+  
 }else if(age == 'Minorenne'){
-
+  
   finalPrice = price * (1 - (discountUnderage/100));
   discountMessage = `Biglietto Young`
-
+      
 }else if (age == 'Over65'){
-
+  
   finalPrice = price * (1 - (discountOver65/100));
   discountMessage = `Biglietto Over65`
 }
@@ -52,10 +45,27 @@ function getRndInteger(min, max) {
 }
 
 
+button1.addEventListener('click', function(){
+  
+  emissioneBiglietto.classList.remove('d-none');
+  emissioneBiglietto.classList.add('d-block');
+  
+});
+
+button2.addEventListener('click', function(){
+  
+  emissioneBiglietto.classList.remove('d-block');
+  emissioneBiglietto.classList.add('d-none');
+  location.reload()
+  
+});
+
+
+
 document.getElementById('output-firstname').innerHTML = firstName;
 document.getElementById('output-lastname').innerHTML = lastName;
 document.getElementById('output-discount').innerHTML = discountMessage;
 document.getElementById('carrozza').innerHTML = Math.floor(Math.random() * 10) + 1;
 document.getElementById('code-tiket').innerHTML = getRndInteger(1000,2000);
-document.getElementById('output-finalprice').innerHTML = finalPrice
+document.getElementById('output-finalprice').innerHTML = finalPrice;
 
